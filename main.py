63a6e759c2886fa87443dc580f1f6d044b28798b
Gautier Dagan <gautierdagan2017@u.northwestern.edu>
2020-03-02 15:35:39 +0000

    enh: simplify + replace pd with set

--- main.py
+++ main.py
@@ -1,84 +1,90 @@
-from datetime import datetime
-import os
-from typing import Optional, List
 import configparser
-
-import pandas as pd
-import requests
-from tqdm import tqdm
+import os
+import pickle
+from typing import Set
 
 from bs4 import BeautifulSoup
-from pydantic import BaseModel, validator
+import requests
 import tweepy
 
 config = configparser.ConfigParser()
 config.read("settings.ini")
 
-CACHE_FOLDER = "cache"
-HISTORY_CSV = "history.csv"
+# cache settings
+CACHE_FOLDER = config["cache"]["cache_folder"]
+CACHE_FILE = config["cache"]["cache_file"]
+CACHE_PATH = f"{CACHE_FOLDER}/{CACHE_FILE}"
+
+# Max number of latest tweets to retrieve per run
+MAX_TWEETS = int(config["twitter"]["max_tweets"])
+
+# Set up the twitter api client using the environment variables from config
+TWITTER_AUTH = tweepy.OAuthHandler(
+    config["twitter"]["consumer_key"], config["twitter"]["consumer_secret"]
+)
+TWITTER_AUTH.set_access_token(
+    config["twitter"]["access_token"], config["twitter"]["access_token_secret"]
+)
+TWITTER_API = tweepy.API(TWITTER_AUTH)
 
 
-class Tweet(BaseModel):
-    """Parsed Favorited Tweet Class"""
-
-    id: int
-    text: str
-    created_at: datetime
-    url: Optional[str]
-
-    @validator("url", always=True, pre=True)
-    def parse_url(cls, v):
-        for url_object in v:
-            full_url = url_object["expanded_url"]
-            if "arxiv" in full_url:
-                return full_url
-        return None
+def read_cache() -> Set[str]:
+    # load cached tweets if exists
+    if os.path.exists(CACHE_PATH):
+        # Return set of already processed urls
+        tweets = pickle.load(open(CACHE_PATH, "rb"))
+        return tweets
+    return set()
 
 
-def get_cached_history() -> pd.DataFrame:
+def save_cache(processed_tweets: Set[str]):
     # create cache folder if non-existent
+    print("Saving cache")
     if not os.path.exists(CACHE_FOLDER):
         os.makedirs(CACHE_FOLDER)
-
-    # load cached tweets if exists
-    if os.path.exists(f"{CACHE_FOLDER}/{HISTORY_CSV}"):
-        tweets = pd.read_csv(f"{CACHE_FOLDER}/{HISTORY_CSV}")
-        return tweets
-
-    return pd.DataFrame()
+    pickle.dump(processed_tweets, open(CACHE_PATH, "wb"))
 
 
-def save_cache_history(tweet_history: pd.DataFrame) -> None:
-    # load cached tweets if exists
-    tweet_history.to_csv(f"{CACHE_FOLDER}/{HISTORY_CSV}", index=False)
+def parse_urls(tweet: tweepy.Status) -> Set[str]:
+    """
+    Parse all the arvix URLs from a tweet object.
+    Returns:
+        urls (Set[str]): set containing all the urls
+    """
+    urls = set()
+    for url_object in tweet.entities["urls"]:
+        full_url = url_object["expanded_url"]
+
+        if "arxiv" in full_url:
+            # parse case when tweet already links to pdf link
+            if "pdf" in full_url:
+                full_url = full_url.replace(".pdf", "").replace("pdf", "abs")
+
+            urls.add(full_url)
+
+    return urls
 
 
-def parse_tweet(tweet: tweepy.Status) -> Tweet:
-    return Tweet(
-        id=tweet.id,
-        text=tweet.text,
-        created_at=tweet.created_at,
-        url=tweet.entities["urls"],
-    )
+def get_tweets() -> Set[str]:
+    """
+    Gets the tweets using tweepy calls
+    Args:
+        max_tweets (int): the number of latest favorited tweets to retrieve
 
-
-def get_recent_tweets() -> List[Tweet]:
-    max_tweets = int(config["twitter"]["max_tweets"])
-
-    auth = tweepy.OAuthHandler(
-        config["twitter"]["consumer_key"], config["twitter"]["consumer_secret"]
-    )
-    auth.set_access_token(
-        config["twitter"]["access_token"], config["twitter"]["access_token_secret"]
-    )
-    api = tweepy.API(auth)
-    tweets = []
-    for fav in tqdm(tweepy.Cursor(api.favorites).items(max_tweets)):
-        tweets.append(parse_tweet(fav))
+    Returns set of unique urls from liked tweets containing `arvix`
+    """
+    tweets = set()
+    for liked_tweet in tweepy.Cursor(TWITTER_API.favorites).items(MAX_TWEETS):
+        tweets |= parse_urls(liked_tweet)
     return tweets
 
 
 def get_arvix_title(arvix_link: str) -> str:
+    """
+    Makes a request to the abstract page of the paper to parse the title of the article
+    """
+    assert "abs" in arvix_link
+
     response = requests.get(arvix_link)
     soup = BeautifulSoup(response.text, "html.parser")
     header_title = soup.find("title").text.split("]")[-1].strip()
@@ -86,18 +92,22 @@ def get_arvix_title(arvix_link: str) -> str:
 
 
 def download_arvix_pdf(arvix_link: str) -> bool:
-    print(f"Downloading link at: {arvix_link}")
+    """
+    Downloads the arvix link (example https://arxiv.org/abs/2002.12345)
+    to pdf in the directory specified by config.
+    First makes a requests to the /abs/ url to get title, then to the pdf
+    url to get the file. Finally saves the file to location using the title as name.
+    """
 
-    if "pdf" in arvix_link:
-        arvix_link = arvix_link.replace(".pdf", "").replace("pdf", "abs")
+    print(f"Downloading link at: {arvix_link}")
 
     # get title of pdf
     title = get_arvix_title(arvix_link)
 
-    # get file path to ibooks folder from settings
+    # get file path to local folder from settings
     pdf_file_path = f"{config['pdf']['pdf_folder_path']}/{title}.pdf"
 
-    # stream download pdf
+    # stream download pdf to desired location
     pdf_dl_link = arvix_link.replace("abs", "pdf")
     response = requests.get(pdf_dl_link, stream=True)
     if response.status_code == 200:
@@ -109,40 +119,27 @@ def download_arvix_pdf(arvix_link: str) -> bool:
         return False
 
 
-def get_full_tweet_history() -> pd.DataFrame:
-    recent_tweets = get_recent_tweets()
-    recent_tweet_history = pd.DataFrame(map(dict, recent_tweets))
+def main() -> None:
+    # gets the set of arvix links from recently liked tweets
+    recent_tweet_set = get_tweets()
+    # gets set of arvix links previously processed
+    cached_tweet_set = read_cache()
 
-    # automatically process tweets without URL
-    recent_tweet_history["processed"] = recent_tweet_history.url.isnull()
+    # Difference between new set and cached set are tweets to download for
+    new_tweets = recent_tweet_set - cached_tweet_set
+    print(f"Found {len(new_tweets)} new tweets.")
+    for new_tweet in new_tweets:
+        success = download_arvix_pdf(new_tweet)
 
-    cached_tweet_history = get_cached_history()
-    if len(cached_tweet_history) > 0:
-        new_tweets = recent_tweet_history.loc[
-            ~recent_tweet_history.id.isin(cached_tweet_history.id)
-        ].copy()
+        # if tweet was downloaded sucessfully
+        if success:
+            cached_tweet_set.add(new_tweet)
+        else:
+            print(f"Error downloading {new_tweet}")
 
-        print(f"Found {len(new_tweets)} new tweets.")
-
-        return pd.concat([cached_tweet_history, new_tweets])
-    else:
-        return recent_tweet_history
-
-
-def process_tweet_history(tweet_history: pd.DataFrame):
-    print("Processing tweet history")
-
-    for _, row in tweet_history[~tweet_history.processed].iterrows():
-        if not row.processed:
-            url = row.url
-            success = download_arvix_pdf(url)
-            if success:
-                tweet_history.loc[row.id == tweet_history.id, "processed"] = True
-
-    print("Saving updated history to cache")
-    save_cache_history(tweet_history)
+    # save the new set to cache
+    save_cache(cached_tweet_set)
 
 
 if __name__ == "__main__":
-    tweet_history = get_full_tweet_history()
-    process_tweet_history(tweet_history)
+    main()
